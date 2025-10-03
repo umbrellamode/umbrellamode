c099533d463ffff62a9c744464c0f9711a70b111
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2025-10-03 18:41:41 -0400

    Refactor director package to use modules

--- packages/director/src/index.ts
+++ packages/director/src/index.ts
@@ -1,11 +1,1 @@
-import { DirectorConfig } from "../types/director.types";
-
-class Director {
-  constructor(args: DirectorConfig = {}) {}
-
-  async act(message: string) {
-    console.log(`Acting... ${message}`);
-  }
-}
-
-export { Director };
+export * from "./director";
--- packages/director/types/index.ts
+++ packages/director/types/index.ts
@@ -1,10 +1,3 @@
 export * from "./act.types";
 export * from "./director.types";
-
-export interface HelloFunctionArgs {
-  name: string;
-}
-
-export interface HelloFunctionResponse {
-  message: string;
-}
+export * from "./document";
