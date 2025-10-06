d6dab1c4cf66cba4ba8b64ac93be0bdb727b134c
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2025-10-06 15:25:50 -0400

    sick

--- packages/director/src/index.ts
+++ packages/director/src/index.ts
@@ -1,1 +1,20 @@
 export * from "./actor";
+export {
+  type BaseActorActionsArgs,
+  type ActorClickActionsArgs,
+  type ActorTypeActionsArgs,
+  type ActorClearActionsArgs,
+  type ActorWaitForElementActionsArgs,
+  type ActorScrollToActionsArgs,
+  type ActorScrollToElementActionsArgs,
+  type ActorScrollToElementTopActionsArgs,
+  type ActorScrollToElementBottomActionsArgs,
+  type ActorScrollByActionsArgs,
+  type ActorScrollPageDownActionsArgs,
+  type ActorScrollPageUpActionsArgs,
+  type ActorFocusActionsArgs,
+  type ActorBlurActionsArgs,
+  type ActorTypeFastActionsArgs,
+  type ActorIsElementInViewportActionsArgs,
+  type ActorGetScrollPositionActionsArgs,
+} from "../types/actor.types";
